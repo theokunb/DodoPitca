837b50375b44eabdb55a6d3c81f69a990dde4476
theokunb <makarovalexeyvery@gmail.com>
2021-11-07 18:19:56 +0900

    Сделали вкладку профили, не получилось задать цвет фреймам, помогите а.

---
+++ DodoPitca/DodoPitca/MVVM/ViewModels/CustomProfileFrameViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DodoPitca.MVVM.ViewModels
+{
+    public class CustomProfileFrameViewModel:BaseViewModel
+    {
+        private string title;
+        private int coins;
+        private ImageSource imagePath;
+        private Brush colorBegin;
+        private Brush colorEnd;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value)
+                    return;
+                title = value;
+                OnpropertyChagned();
+            }
+        }
+        public int Coins
+        {
+            get => coins;
+            set
+            {
+                if (coins == value)
+                    return;
+                coins = value;OnpropertyChagned();
+            }
+        }
+        public ImageSource ImagePath
+        {
+            get => imagePath;
+            set
+            {
+                if (imagePath == value)
+                    return;
+                imagePath = value;
+                OnpropertyChagned();
+            }
+        }
+    }
+}
---
+++ DodoPitca/DodoPitca/MVVM/Views/CustomProfileFrame.xaml.cs
@@ -0,0 +1,60 @@
+﻿using DodoPitca.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DodoPitca.MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomProfileFrame : ContentView
+    {
+        public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(CustomProfileFrame), "", BindingMode.TwoWay, propertyChanged: TitlePropertyChanged);
+        public static readonly BindableProperty CoinsProperty = BindableProperty.Create(nameof(Coins), typeof(int), typeof(CustomProfileFrame), 0, BindingMode.TwoWay, propertyChanged: CoinsPropertyChanged);
+        public static readonly BindableProperty ImagePathProperty = BindableProperty.Create(nameof(ImagePath), typeof(ImageSource), typeof(CustomProfileFrame), null, BindingMode.TwoWay, propertyChanged: ImagePathPropertyChanged);
+        
+        public string Title
+        {
+            get { return GetValue(TitleProperty).ToString(); }
+            set { SetValue(TitleProperty, value); }
+        }
+        public int Coins
+        {
+            get { return (int)GetValue(CoinsProperty); }
+            set { SetValue(CoinsProperty, value); }
+        }
+        public ImageSource ImagePath
+        {
+            get { return (ImageSource)GetValue(ImagePathProperty); }
+            set { SetValue(ImagePathProperty, value); }
+        }
+
+        private static void TitlePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomProfileFrame)bindable;
+            var controlVM = control.BindingContext as CustomProfileFrameViewModel;
+            controlVM.Title = newValue.ToString();
+        }
+
+        private static void CoinsPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomProfileFrame)bindable;
+            var controlVM = control.BindingContext as CustomProfileFrameViewModel;
+            controlVM.Coins += Convert.ToInt32(newValue);
+        }
+        private static void ImagePathPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomProfileFrame)bindable;
+            var controlVM = control.BindingContext as CustomProfileFrameViewModel;
+            controlVM.ImagePath = (ImageSource)newValue;
+        }
+        public CustomProfileFrame()
+        {
+            InitializeComponent();
+        }
+    }
+}
