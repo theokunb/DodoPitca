6ccb53e661eb38d43937671063d4dd376340de09
theokunb <theokunb@gmail.com>
2021-11-08 12:18:49 +0900

    Сделали страницу контакты

---
+++ DodoPitca/DodoPitca/MVVM/ViewModels/CustomContactsButtonViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DodoPitca.MVVM.ViewModels
+{
+    public class CustomContactsButtonViewModel : BaseViewModel
+    {
+        private string text;
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+                if (text == value)
+                    return;
+                text = value; OnpropertyChagned();
+            }
+        }
+    }
+}
---
+++ DodoPitca/DodoPitca/MVVM/Views/CustomContactsButton.xaml.cs
@@ -0,0 +1,37 @@
+﻿using DodoPitca.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DodoPitca.MVVM.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomContactsButton : ContentView
+    {
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text),typeof(string),typeof(CustomContactsButton),"",BindingMode.TwoWay,propertyChanged:TextPropertyChanged);
+
+
+        public string Text
+        {
+            get {return GetValue(TextProperty).ToString(); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void TextPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CustomContactsButton)bindable;
+            var controlVM = control.BindingContext as CustomContactsButtonViewModel;
+            controlVM.Text = newValue.ToString();
+        }
+
+        public CustomContactsButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
