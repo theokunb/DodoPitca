2a52277715656d1a1a87d926c7e5b96fa418fdd3
theokunb <makarovalexeyvery@gmail.com>
2021-11-12 21:35:47 +0900

    пофиксили поиск по товарам.

--- DodoPitca/DodoPitca/MVVM/Views/PageSearchTovar.xaml.cs
+++ DodoPitca/DodoPitca/MVVM/Views/PageSearchTovar.xaml.cs
@@ -15,21 +15,50 @@ namespace DodoPitca.MVVM.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PageSearchTovar : ContentPage
     {
-        private IList<View> tovars;
+        private View[] tovars;
         public PageSearchTovar(IList<View> views)
         {
             InitializeComponent();
-            tovars = views;
+            tovars = new View[views.Count];
+            views.CopyTo(tovars, 0);
         }
 
         private void Entry_TextChanged(object sender, TextChangedEventArgs e)
         {
-            stackTovars.Children.Clear();
-            Regex reg = new Regex("^" + (BindingContext as PageSearchTovarViewModel).SearchPattern + @"\S+$");
-            for(int i = 0; i < tovars.Count; i++)
+
+            if ((sender as Entry).Text == string.Empty)
             {
-                if (reg.IsMatch((tovars[i] as CustomTovar).Title))
-                    stackTovars.Children.Add(tovars[i]);
+                stackTovars.Children.Clear();
+                return;
+            }
+            Regex reg = new Regex("^" + (BindingContext as PageSearchTovarViewModel).SearchPattern + @".+$", RegexOptions.IgnoreCase);
+            for(int i = 0; i < tovars.Length; i++)
+            {
+                for(int j = stackTovars.Children.Count - 1; j >= 0; j--)
+                {
+                    if (stackTovars.Children.Contains(stackTovars.Children[j]) && !reg.IsMatch((stackTovars.Children[j] as CustomTovar).Title))
+                        stackTovars.Children.Remove(stackTovars.Children[j]);
+                }
+                if(reg.IsMatch((tovars[i] as CustomTovar).Title))
+                {
+                    bool povtorenie = false;
+                    for(int k = 0; k < stackTovars.Children.Count; k++)
+                    {
+                        if ((stackTovars.Children[k] as CustomTovar).Title == (tovars[i] as CustomTovar).Title)
+                            povtorenie = true;
+                        if (povtorenie)
+                            break;
+                    }
+                    if (povtorenie)
+                        continue;
+                    stackTovars.Children.Add(new CustomTovar()
+                    {
+                        Title = (tovars[i] as CustomTovar).Title,
+                        Description = (tovars[i] as CustomTovar).Description,
+                        Price = (tovars[i] as CustomTovar).Price,
+                        ImagePath = (tovars[i] as CustomTovar).ImagePath
+                    });
+                }
             }
         }
     }
